8d2c8e4685ee8b417c6370fceab8625af0e1c32c
Daniel Vargas <davarg5@yahoo.com>
2020-10-16 12:39:37 -0400

    inputs working

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import React, { useState, useEffect } from 'react';
-import { Route, Link } from 'react-router-dom'
-import Form from './Form'
+import { Route, Link } from 'react-router-dom';
+import Form from './Form';
+import * as yup from 'yup';
+import axios from 'axios';
+import styled from 'styled-components'
+
+const Navigation = styled.nav`
+  display: flex;
+  justify-content: space-around;
+`
+
+const NavLinks = styled.div`
+  align-items: right;
+  display: flex;
+  justify-content: space-around;
+  margin-top: 2%;
+`
 
 const App = () => {
 
+  
   const initialFormValues = {
     name: '',
     size: '',
@@ -17,7 +33,12 @@ const App = () => {
 
   const initialFormErrors = {
     name: '',
-    size: ''
+    size: '',
+    pepperoni: '',
+    sausage: '',
+    ham: '',
+    bacon: '',
+    special_instructions: ''
   }
 
   const [formValues, setFormValues] = useState(initialFormValues);
@@ -25,12 +46,47 @@ const App = () => {
   const [formErrors, setFormErrors] = useState(initialFormValues);
   const [disabled, setDisabled] = useState(true);
 
-  const change = (inputName, inputValue) => {
+  const formSchema = yup.object().shape({
+    name: yup.string().required('Name is required').min(2,'Name must be at least 2 characters'),
+    size: yup.string().oneOf(['small', 'medium', 'large'], 'Size is required'),
+    pepperoni: yup.boolean(),
+    sausage: yup.boolean(),
+    ham: yup.boolean(),
+    bacon: yup.boolean(),
+    special_instructions: yup.boolean()
+  });
 
+  const change = (inputName, inputValue) => {
+    yup.reach(formSchema, inputName).validate(inputValue)
+      .then(() => {
+        setFormErrors({
+          ...formErrors,
+          [inputName]: ''
+        })
+      })
+      .catch((err) => {
+        setFormErrors({
+          ...formErrors,
+          [inputName]: err.errors[0]
+        })
+      })
     setFormValues({
       ...formValues,
       [inputName]: inputValue
     })
+  }
+
+
+  const postNewUser = newOrder => {
+    axios.post('https://reqres.in/api/users', newOrder)
+      .then( res => {
+        setOrders([res.data, 
+          ...orders]);
+        setFormValues(initialFormValues);
+      })
+      .catch( err => {
+        console.log(err);
+      })
   }
 
   const submit = () => {
@@ -42,16 +98,26 @@ const App = () => {
       }),
       special_instructions: formValues.special_instructions.trim()
     }
-    //postNewUser(newOrder);
+    postNewUser(newOrder);
   }
+
+  useEffect(() => {
+    formSchema.isValid(formValues)
+      .then((valid) => {
+        setDisabled(!valid);
+      })
+  }, [formValues])
+  
 
   return (
     <div className="App">
-      <h1>Lambda Eats</h1>
-      <nav>
+      <Navigation>
+        <h1>Lambda Eats</h1>
+        <NavLinks>
         <Link to='/'><div>Home</div></Link>
         <Link to='/pizza'><div>Order</div></Link>
-      </nav>
+        </NavLinks>
+      </Navigation>
       <Route exact path='/'>
 
       </Route>
@@ -63,6 +129,13 @@ const App = () => {
         disabled={disabled}
         errors={formErrors}
         />
+        {orders.map(order => {
+          return (
+            <div>
+              Hello
+            </div>
+          )
+        })}
       </Route>
     </div>
   );
--- src/Form.js
+++ src/Form.js
@@ -1,4 +1,22 @@
 import React from 'react';
+import styled from 'styled-components'
+
+const FormBody = styled.div`
+    border: 10px solid black;
+`
+
+const FormInputs = styled.div`
+    background-color: lightgray;
+    padding: 0.5% 0% 0.5% 3%;
+    margin-bottom: 2%;
+    margin-top: 1%;
+`
+
+const PizzaImg = styled.img`
+    width: 90%;
+    margin-left: 5%;
+    border: 2px solid gray;
+`
 
 export default function Form(props) {
     const { values, submit, change, disabled, errors } = props;
@@ -9,18 +27,23 @@ export default function Form(props) {
     }
 
     const onChange = evt => {
-
+        const { name, value, type, checked } = evt.target;
+        const valueToUse = type === 'checkbox' ? checked : value;
+        change(name, valueToUse);
     }
 
     return (
-        <div>
+        <FormBody>
             <h2>Build Your Own Pizza</h2>
-            <img src=''/>
+            <PizzaImg src='https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-homemade-pizza-horizontal-1542312378.png?crop=1.00xw:0.752xh;0,0.139xh&resize=640:*'></PizzaImg>
             <h2>Build Your Own Pizza</h2>
-        <form>
+        <form onSubmit={onSubmit}>
 
+            <FormInputs>
             <h3>Choice of Size</h3>
             <p>Required</p>
+            <div>{errors.size}</div>
+            </FormInputs>
             <select name='size' value={values.role} onChange={onChange}>
                 <option value=''>---Select a size----</option>
                 <option value='small'>Small</option>
@@ -28,8 +51,10 @@ export default function Form(props) {
                 <option value='large'>Large</option>
             </select>
 
+            <FormInputs>
             <h3>Choice of Toppings</h3>
             <p>Choose up to 4</p>
+            </FormInputs>
             <div>Pepperoni</div>
             <input
             type='checkbox'
@@ -59,9 +84,33 @@ export default function Form(props) {
             onChange={onChange}
             />
             
+            <FormInputs>
+            <h3>Special Instructions</h3>
+            </FormInputs>
+            <input
+            type='text'
+            name='special_instructions'
+            value={values.special_instructions}
+            onChange={onChange}
+            placeholder='Anything else you want to add?'
+            />
 
+            <FormInputs>
+            <h3>Name</h3>
+            </FormInputs>
+            <div>{errors.name}</div>
+            <input
+            type='text'
+            name='name'
+            value={values.name}
+            onChange={onChange}
+            />
+            
+            <div>
+            <button disabled={disabled}>Submit Order</button>
+            </div>
         </form>
         
-        </div>
+        </FormBody>
     )
 }
